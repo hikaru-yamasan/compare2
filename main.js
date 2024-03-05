9b90bc4061a58b3ef9fdb2dc6760da53fd17e9ef
hikaru-yamasan <h.yamaguchi.2121@gmail.com>
2024-03-06 02:21:35 +0900

    add main-process & renderer-process

--- main.js
+++ main.js
@@ -1,18 +1,38 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain, dialog } = require('electron')
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 600,
     height: 400,
-    title: 'マイアプリ',
+    title: 'Compare',
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
   })
 
-  mainWindow.webContents.openDevTools({ mode: 'detach' })
-  mainWindow.loadFile('index.html')
+  // 同時にデベロッパーツールを起動する
+  // mainWindow.webContents.openDevTools({ mode: 'detach' })
+  mainWindow.loadFile('index.html') // index.html を読み込ませている -> Chromium が対応
+
+  ipcMain.handle('open-dialog', async (_e, _arg) => {
+    return dialog
+      .showOpenDialog(mainWindow, {
+        properties: ['openFile'],
+      })
+      .then((result) => {
+        if (result.canceled) return ''
+
+        return result.filePaths[0]
+      })
+      .catch((err) => console.error(err))
+  })
 }
 
+// app はアプリ全体のライフサイクルを制御するモジュール
 app.once('ready', () => {
   createWindow()
 })
 
+// すべてのウィンドウが閉じられたというイベントに対応してアプリ自身を終了させる
 app.once('window-all-closed', () => app.quit())
